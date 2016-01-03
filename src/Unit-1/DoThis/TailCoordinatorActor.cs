17f7097c832ed9acb9a38beaf74c2106f3235e4c
ela j <benzodwuazepina@interia.pl>
2016-01-03 20:41:49 +0100

    completed lesson 1.4

--- src/Unit-1/DoThis/TailCoordinatorActor.cs
+++ src/Unit-1/DoThis/TailCoordinatorActor.cs
@@ -43,9 +43,32 @@ namespace WinTail
             if (message is StartTail)
             {
                 var msg = message as StartTail;
-                // YOU NEED TO FILL IN HERE
+                // here we are creating our first parent/child relationship!
+                // the TailActor instance created here is a child
+                // of this instance of TailCoordinatorActor
+                Context.ActorOf(Props.Create(() => new TailActor(msg.ReporterActor, msg.FilePath)));
             }
 
+        }
+
+        // TailCoordinatorActor.cs
+        protected override SupervisorStrategy SupervisorStrategy()
+        {
+            return new OneForOneStrategy(
+                10, // maxNumberOfRetries
+                TimeSpan.FromSeconds(30), // duration
+                x =>
+                {
+            //Maybe we consider ArithmeticException to not be application critical
+            //so we just ignore the error and keep going.
+            if (x is ArithmeticException) return Directive.Resume;
+
+            //Error that we cannot recover from, stop the failing actor
+            else if (x is NotSupportedException) return Directive.Stop;
+
+            //In all other cases, just restart the failing actor
+            else return Directive.Restart;
+                });
         }
     }
 }
